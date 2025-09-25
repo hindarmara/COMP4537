4a3dcaeba2380e62a21cc6ca288b2119ffd43a04
hindarmara <hindarmara@gmail.com>
2025-09-25 01:03:39 -0700

    working read and write file.txt

---
+++ labs/3/readWriteFile/modules/utils.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const messages = require("../lang/en/en.json");
+
+class Utils {
+  static sendResponse(response, statusCode, contentType, message) {
+    response.writeHead(statusCode, { "Content-Type": contentType });
+    response.end(message);
+  }
+
+  static appendToFile(filePath, content) {
+    fs.appendFileSync(filePath, content + "\n", "utf8");
+  }
+
+  static showFileContent(filePath, res) {
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    Utils.sendResponse(res, 200, "text/plain", fileContent);
+  }
+
+  static fileExists(filePath) {
+    return fs.existsSync(filePath);
+  }
+
+  static invalidRoute(res) {
+    Utils.sendResponse(
+      res,
+      404,
+      "text/html",
+      `<html><body><h3>${messages.invalidRoute}</h3></body></html>`
+    );
+  }
+
+  static checkRoute(receivedRoute, expectedRoute) {
+    return receivedRoute === expectedRoute;
+  }
+
+  static fileNotFound(res) {
+    Utils.sendResponse(
+      res,
+      404,
+      "text/html",
+      `<html><body><h3>${messages.fileNotFound}</h3></body></html>`
+    );
+  }
+}
+
+module.exports = Utils;
---
+++ labs/3/readWriteFile/server.js
@@ -0,0 +1,87 @@
+const path = require("path");
+const http = require("http");
+const url = require("url");
+const Utils = require("./modules/utils");
+const messages = require("./lang/en/en.json");
+
+const FILE_NAME = "file.txt";
+
+class AppServer {
+  constructor() {
+    this.port = process.env.PORT || 8000;
+    this.writerRoute = "/COMP4537/labs/3/writeFile/";
+    this.readerRoute = "/COMP4537/labs/3/readFile/";
+    this.filePath = path.join(__dirname, FILE_NAME);
+    this.fileName = FILE_NAME;
+  }
+
+  handleWriterRequest(res, receivedRoute, queryText) {
+    if (receivedRoute === this.writerRoute) {
+      Utils.appendToFile(this.filePath, queryText);
+      Utils.sendResponse(
+        res,
+        200,
+        "text/html",
+        `<html><body><h3>${messages.fileWritten}</h3></body></html>`
+      );
+    } else {
+      Utils.invalidRoute(res);
+    }
+  }
+
+  handleReaderRequest(res, receivedRoute) {
+    console.log(`Received route: ${receivedRoute}`);
+
+    const routeParts = receivedRoute.split("/");
+    const receivedFileName = routeParts[routeParts.length - 1];
+    console.log(`Requested file: ${receivedFileName}`);
+
+    if (receivedFileName === this.fileName) {
+      console.log(`File path: ${this.filePath}`);
+
+      Utils.showFileContent(this.filePath, res);
+    } else {
+      Utils.invalidRoute(res);
+    }
+  }
+
+  appMode(req, res, receivedRoute, queryText) {
+    switch (true) {
+      case req.url.startsWith(this.writerRoute):
+        this.handleWriterRequest(res, receivedRoute, queryText);
+        break;
+
+      case req.url.startsWith(this.readerRoute):
+        if (!Utils.fileExists(this.filePath)) {
+          Utils.fileNotFound(res);
+          return;
+        }
+        this.handleReaderRequest(res, receivedRoute);
+        break;
+
+      default:
+        Utils.invalidRoute(res);
+    }
+  }
+
+  start() {
+    http
+      .createServer((req, res) => {
+        const parsedUrl = url.parse(req.url, true);
+        const receivedRoute = parsedUrl.pathname;
+        const queryText = parsedUrl.query.text || messages.noQuery;
+
+        console.log(`Received URL: ${req.url}`);
+        console.log(`Received route: ${receivedRoute}`);
+        console.log(`Received query: ${queryText}`);
+
+        this.appMode(req, res, receivedRoute, queryText);
+      })
+      .listen(this.port);
+
+    console.log(`Server running at port: ${this.port}`);
+  }
+}
+
+const app = new AppServer();
+app.start();
