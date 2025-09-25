4481a56e2137b2aba651a3c4db35b2985e4a31d1
hindarmara <hindarmara@gmail.com>
2025-09-24 18:43:21 -0700

    fix route to /COMP4537/labs/3/getDate/?name=YourName

--- labs/3/getDate/server.js
+++ labs/3/getDate/server.js
@@ -4,6 +4,7 @@ const utils = require("./modules/utils.js");
 const messages = require("./lang/en/en.json");
 
 const PORT = process.env.PORT || 8000;
+const ROUTE = "/COMP4537/labs/3/getDate/";
 
 // https://yourDomainName.xyz/COMP4537/labs/3/getDate/?name=John
 const server = http.createServer((req, res) => {
@@ -13,23 +14,37 @@ const server = http.createServer((req, res) => {
   console.log(`Path: ${path}`);
   console.log(`Query: ${JSON.stringify(reqUrl.query)}`);
 
-  const name = reqUrl.query.name || "Stranger";
-  console.log(`Name: ${name}`);
+  if (path !== ROUTE) {
+    const message = messages["404"] + " " + messages["tryRoute"];
+    console.log(`Message: ${message}`);
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end(
+      `<html>
+        <body>
+          <p>${message}</p>
+        </body>
+      </html>`
+    );
+    return;
+  } else {
+    const name = reqUrl.query.name || messages['noName'];
+    console.log(`Name: ${name}`);
 
-  const dateTimeNow = utils.getDateTimeNow();
-  console.log(`Date and time now: ${dateTimeNow}`);
+    const dateTimeNow = utils.getDateTimeNow();
+    console.log(`Date and time now: ${dateTimeNow}`);
 
-  const message = messages.message.replace("%1", name).replace("%2", dateTimeNow);
-  console.log(`Message: ${message}`);
+    const message = messages.message.replace("%1", name).replace("%2", dateTimeNow);
+    console.log(`Message: ${message}`);
 
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(
-    `<html>
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(
+      `<html>
       <body>
         <H3>${message}</H3>
       </body>
     </html>`
-  );
+    );
+  }
 });
 
 server.listen(PORT);
